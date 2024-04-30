d40a21fcd4221b85c8540ce7b7212f934171d995
gunminiho <gunminiho@gmail.com>
2024-04-30 16:19:56 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import Rose from "./rose"
+import Foto from "./assets/foto.jpeg"
+
+function App() {
+ 
+
+  return (
+    <>
+     <div id="total" className='total'>
+     <img src={Foto} />
+    <Rose/>
+    <div>
+    <p>En este día del psicólogo quiero recordarte lo especial que eres para mí. Tu amor y tu apoyo son el centro de mi. Tu pasión por la psicologia es un reflejo de tu increíble corazón y tu dedicación para ayudar a los demás.
+    Te amo más de lo que las palabras pueden expresar. Que hoy y siempre sientas todo el amor y la admiración que tengo por ti.
+</p>
+    </div>
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/rose.jsx
@@ -0,0 +1,47 @@
+import "./rose.css";
+
+const Rose = () => {
+    return (
+        <>
+    <div class="container">
+	<div class="glass"></div>
+	<div class="thorns">
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+	</div>
+	<div class="glow"></div>
+	<div class="rose-leaves">
+		<div></div>
+		<div></div>
+	</div>
+	<div class="rose-petals">
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+	</div>
+	<div class="sparkles">
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+	</div>
+    </div>
+    
+    </>
+    );
+};
+    
+
+export default Rose;
